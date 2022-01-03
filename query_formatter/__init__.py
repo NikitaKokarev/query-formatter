fd092298a081f1da379fbaac71e948edd8f0e4a4
nv.kokarev <nv.kokarev@tensor.ru>
2022-01-03 23:09:45 +0300

    build __init__

---
+++ query_formatter/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+ 
+"""
+INIT QUERY FORMATTER
+~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2021 by Nikita Kokarev.
+:license: GNU General Public License v3 (GPLv3), see LICENSE for more details.
+"""
+__author__ = 'kokarev.nv'
+
+from .query_formatter import cast_to_type, SqlEscaper, QueryFormatter
